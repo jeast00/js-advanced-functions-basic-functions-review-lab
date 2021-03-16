0cabf6a9331519e25d6645b7b27750e10fa49cac
jeast00 <38165949+jeast00@users.noreply.github.com>
2021-03-16 12:22:20 -0400

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+let mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = '*') {
+    return function(string = 'special') {
+        return `You are ${flair}${string}${flair}!`;
+    }
+}
+
+let Calculator = {
+    add: function(a, b) {
+        return a + b;
+    },
+
+    subtract: function (a, b) {
+        return a - b;
+    },
+
+    multiply: function (a, b) {
+        return a * b;
+    },
+
+    divide: function (a, b) {
+        return a / b;
+    }
+}
+
+function actionApplyer(num, aOS) {
+    let number = num;
+
+    for(let i = 0; i < aOS.length; i++) {
+        number = aOS[i](number);
+    }
+
+    return number;
+}
